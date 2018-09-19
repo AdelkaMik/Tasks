5ed3e81b1c714b607f55e0aa2047eb22a3933ae2
Adéla Mikešová <adelkamik98@gmail.com>
2018-09-19 18:35:05 +0200

    Muj prvni commit

---
+++ Car.java
@@ -0,0 +1,23 @@
+public class Car extends Vehicle{
+    String licensePlate;
+    int count;
+
+    public Car{
+        this.licensePlate = "Unknown";
+        this.count = 0;
+    }
+    
+    public Car(String s, int i){
+        this.s = licensePlate;
+        
+    }
+    
+    @override
+    public washwheels {
+        System.out.println("Car cleaned");
+    }
+
+    public statict getLicenseplate{
+        return licensePlate;
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+public abstract class Vehicle{
+    int wheels;
+
+    public Vehicle(int w){
+        this.w = wheels;
+    }
+
+    public static getWheels{
+        return wheels;
+    }
+
+    public abstract washWheels;
+    
+
+}
